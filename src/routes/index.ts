5ff4272e463c89d2f44b8faa5c4c7f8ae7544f27
Leonardo Santos <lcds90@gmail.com>
2021-06-25 14:17:13 -0300

    home controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,14 @@ import { getProductsController } from '@useCases/GetProducts'
 
 const router = Router()
 
+router.get('/', (request, response) => {
+  response.json({
+    intro: 'Projeto de lista de catalogo de itens para empresa ST IT Cloud. Links para acessar:',
+    products: 'GET - https://api-catalog-list.herokuapp.com/products/:organizationName?tags=<tag><tag>',
+    login: 'POST - https://api-catalog-list.herokuapp.com/login'
+  })
+})
+
 router.post('/login', (request, response) => {
   return authUserController.handle(request, response)
 })
