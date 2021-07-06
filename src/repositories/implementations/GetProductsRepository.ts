cbc889de86103b2617662cc30ad193b5e18effc7
Leonardo Santos <lcds90@gmail.com>
2021-07-06 02:45:05 -0300

    refactoring getProducts

--- src/repositories/implementations/GetProductsRepository.ts
+++ src/repositories/implementations/GetProductsRepository.ts
@@ -8,132 +8,26 @@ const readline = require('readline');
 import { Product } from '@entities/Product';
 import { IProductsRepository } from '@repositories/IProductsRepository';
 
+// SECTION Get - Produtos e Ordenação
 export class GetProductsRepository implements IProductsRepository {
-  /* async validateRoleAndVerifyProduct(
-    organizationName: string,
-    role: string,
-    products: Product[]
-  ): Promise<Product[]> | undefined {
-    for(const product of products){
-      product.name
+  async checkLevelStuff(product: Product, role: string) {
+    // NOTE senior - level 0, 1 e 2, acesso a todos niveis
+    if (role === 'senior') return product;
+    
+    // NOTE middle - level 1 e 2
+    if (role === 'middle'){
+      
     }
-  } */
-
-  async generateSubCategory(
-    queries: string[],
-    products: Product[],
-    level: number,
-    parent: string,
-    letter: string
-  ): Promise<any[]> {
-    const filteredItems = await products.filter((product) => {
-      return queries.includes(product.department);
-    });
-
-    filteredItems.unshift({
-      name: `${parent}`,
-      level: 1,
-      parent: `${letter}`,
-    });
-
-    return filteredItems;
+    // junior - level 2
+    // intern - level 0, 1 e 2, porém somente sob a organization STUFF A
   }
 
-  async generateListOrganization(products: Product[]) {
-    const parentStuff_A01 = await this.generateSubCategory(
-      ['Books', 'Movies', 'Music'],
-      products,
-      2,
-      'STUFF A01',
-      'STUFF A'
+  async generateListOrganization(products: Product[], role: string) {
+    const isRoleValidWithProductsList = products.map((product) =>
+      this.checkLevelStuff(product, role)
     );
-    const parentStuff_A02 = await this.generateSubCategory(
-      ['Games', 'Electronics', 'Computers'],
-      products,
-      2,
-      'STUFF A02',
-      'STUFF A'
-    );
-    const parentStuff_A03 = await this.generateSubCategory(
-      ['Home', 'Garden', 'Tools', 'Clothing'],
-      products,
-      2,
-      'STUFF A03',
-      'STUFF A'
-    );
-    const parentStuff_B01 = await this.generateSubCategory(
-      ['Grocery', 'Health'],
-      products,
-      2,
-      'STUFF B01',
-      'STUFF B'
-    );
-    const parentStuff_B02 = await this.generateSubCategory(
-      ['Beauty', 'Toys'],
-      products,
-      2,
-      'STUFF B02',
-      'STUFF B'
-    );
-    const parentStuff_B03 = await this.generateSubCategory(
-      ['Kids'],
-      products,
-      2,
-      'STUFF B03',
-      'STUFF B'
-    );
-    const parentStuff_C01 = await this.generateSubCategory(
-      ['Baby', 'Shoes'],
-      products,
-      2,
-      'STUFF C01',
-      'STUFF C'
-    );
-    const parentStuff_C02 = await this.generateSubCategory(
-      ['Jewelery', 'Sports', 'Outdoors'],
-      products,
-      2,
-      'STUFF C02',
-      'STUFF C'
-    );
-    const parentStuff_C03 = await this.generateSubCategory(
-      ['Automotive', 'Industrial'],
-      products,
-      2,
-      'STUFF C03',
-      'STUFF C'
-    );
-
-    const stuffA = await [].concat(
-      ...parentStuff_A01,
-      ...parentStuff_A02,
-      ...parentStuff_A03
-    );
-    const stuffB = await [].concat(
-      ...parentStuff_B01,
-      ...parentStuff_B02,
-      ...parentStuff_B03
-    );
-    const stuffC = await [].concat(
-      ...parentStuff_C01,
-      ...parentStuff_C02,
-      ...parentStuff_C03
-    );
-
-    stuffA.unshift({
-      name: 'STUFF A',
-      level: 0,
-    });
-    stuffB.unshift({
-      name: 'STUFF B',
-      level: 0,
-    });
-    stuffC.unshift({
-      name: 'STUFF C',
-      level: 0,
-    });
-
-    return [].concat(...stuffA, ...stuffB, ...stuffC);
+    if (isRoleValidWithProductsList) return products;
+    return undefined;
   }
 
   async findProducts(
@@ -141,43 +35,47 @@ export class GetProductsRepository implements IProductsRepository {
     user_tags: string[],
     roles: string[]
   ): Promise<Product[]> {
-    const products: Product[] = [];
     const filesDir = `${dirname()}/fixtures/`;
     const role = roles[0];
-    const files = (await readdir(filesDir)).filter(
-      (item) => !!!~item.indexOf('.json')
-    );
 
-    const streamFile = createReadStream(join(filesDir, files[0]));
+    const streamFile = createReadStream(join(filesDir, 'products.txt'));
 
+    // NOTE Criando interface com a biblioteca readline para percorrer linha de linha do arquivo products.txt
     const rl = await readline.createInterface({
       input: streamFile,
     });
+    const products: Product[] = [];
 
     for await (const line of rl) {
+      // NOTE Filtrando linha percorrida do arquivo e transformando em um objeto
       let filteredProduct = JSON.parse(line);
+
+      // NOTE Verificando organização
       if (
         filteredProduct.department.toLowerCase() ==
         organizationName.toLowerCase()
       ) {
+        // NOTE Verificando a existência de tags, pois a cada uma que percorrer e a combinação estar correta será acrescentado ao Array products
         if (user_tags.length > 0) {
-          filteredProduct.tags.filter((tag) => {
-            user_tags.filter((user_tag) => {
-              if (tag.toLowerCase() === user_tag.toLowerCase()) {
+          for (const tag of filteredProduct.tags) {
+            user_tags.map((userTag) => {
+              if (userTag.toLowerCase() === tag.toLowerCase())
                 products.push(filteredProduct);
-              }
             });
-          });
+          }
         } else {
+          // NOTE Caso não tenha tags, irá realizar o filtro com somente os itens da categoria
           products.push(filteredProduct);
         }
       }
     }
+    // NOTE Caso tenha sucesso no retorno de produtos retorna a lista com a verificação de cada item com o cargo atual do usuário
     if (products.length > 0) {
-      const list = await this.generateListOrganization(products);
+      const list = await this.generateListOrganization(products, role);
       return list;
     }
-    // const isValid = await this.validateRoleAndVerifyProduct(organizationName, role, list);
+
+    // NOTE Retorno para acionamento de erro do useCase, caso nenhum parametro correspondeu a busca
     return undefined;
   }
 }
